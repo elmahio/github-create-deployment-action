4406c055513658b3218cc89b02ad94aa713441c2
Thomas Ardal <thomasardal@gmail.com>
2021-08-10 14:53:33 +0200

    Re-added script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 ## upload
 
-#curl --location --request POST "https://api.elmah.io//v3/deployments?api_key=$1" \
-#--header 'Content-Type: application/json' \
-#--data-raw '{
-#    "version": $2,
-#    "description": "$3",
-#    "userName": "$4",
-#    "userEmail": "$5",
-#    "logId": "6"
-#}'
+curl --location --request POST "https://api.elmah.io//v3/deployments?api_key=$1" \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "version": $2,
+    "description": "$3",
+    "userName": "$4",
+    "userEmail": "$5",
+    "logId": "6"
+}'
