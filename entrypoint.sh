6fc681117f01d1e333a0a0170d4ab9b32fc2b98c
Thomas Ardal <thomasardal@gmail.com>
2021-08-10 15:15:31 +0200

    Re-added all parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ sh -c "echo $*"
 
 curl --location --request POST "https://api.elmah.io//v3/deployments?api_key=$1" \
 --header 'Content-Type: application/x-www-form-urlencoded' \
---data "version=$2&description=$3&logId=$6"
+--data "version=$2&description=$3&userName=$4&userEmail=$5&logId=$6"
