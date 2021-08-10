8a920a977a4109b9de0eccb2974acbd328250d47
Thomas Ardal <thomasardal@gmail.com>
2021-08-10 14:55:45 +0200

    Parameters fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@
 curl --location --request POST "https://api.elmah.io//v3/deployments?api_key=$1" \
 --header 'Content-Type: application/json' \
 --data-raw '{
-    "version": $2,
+    "version": "$2",
     "description": "$3",
     "userName": "$4",
     "userEmail": "$5",
-    "logId": "6"
+    "logId": "$6"
 }'
